5e7444509df9be57d5400ab9372eb66ef95bb89f
shoumitro-cse <shoumitro26@gmail.com>
2022-06-24 23:38:21 +0600

    initial commit

---
+++ weather_advice_product_app/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for weather_advice_product_app project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather_advice_product_app.settings')
+
+application = get_wsgi_application()
